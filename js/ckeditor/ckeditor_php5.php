425d42e236ae7f3ae7dfae87b9ba4cc7d4bfb7aa
Chris Watt <chris@network-13.com>
2010-09-21 22:42:47 +0000

    Reverting to old ckeditor, 3.4.1 has a problem with chrome inserting images

--- js/ckeditor/ckeditor_php5.php
+++ js/ckeditor/ckeditor_php5.php
@@ -20,11 +20,11 @@ class CKEditor
 	/**
 	 * The version of %CKEditor.
 	 */
-	const version = '3.4.1 (SVN)';
+	const version = '3.3.1';
 	/**
 	 * A constant string unique for each release of %CKEditor.
 	 */
-	const timestamp = 'A8J74JI';
+	const timestamp = 'A5AB4B6';
 
 	/**
 	 * URL to the %CKEditor installation directory (absolute or relative to document root).
@@ -78,7 +78,7 @@ class CKEditor
 	 * A string indicating the creation date of %CKEditor.
 	 * Do not change it unless you want to force browsers to not use previously cached version of %CKEditor.
 	 */
-	public $timestamp = "A8J74JI";
+	public $timestamp = "A5AB4B6";
 	/**
 	 * An array that holds event listeners.
 	 */
